09126b431d32678b6e1a3de1b8bd9b51d4a7f4c3
Rsharma0374 <61918365+Rsharma0374@users.noreply.github.com>
2026-01-19 15:12:55 +0530

    Update server IP for Kubernetes deployment script

--- jenkins-deployment-script/auth-service/deploy-k8s.sh
+++ jenkins-deployment-script/auth-service/deploy-k8s.sh
@@ -5,7 +5,7 @@ echo "=== Starting Document Utility Service Kubernetes Deployment ==="
 
 PASSWORD=$1  # First argument passed to the script
 BRANCH=$2
-SERVER_IP="161.118.166.22"
+SERVER_IP="80.225.218.113"
 REPO_NAME="userAuthentication"
 DOCKER_IMAGE_NAME="auth-service"
 DOCKER_IMAGE_TAG="latest"
