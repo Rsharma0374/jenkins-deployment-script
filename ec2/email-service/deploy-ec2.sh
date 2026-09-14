5250b25c8aa2e215f86c5e6d02f08f4ce4348ef6
Rsharma0374 <61918365+Rsharma0374@users.noreply.github.com>
2026-09-14 14:08:51 +0530

    Change log file path for remote deployment

--- ec2/email-service/deploy-ec2.sh
+++ ec2/email-service/deploy-ec2.sh
@@ -23,7 +23,7 @@ LOCAL_JAR_GLOB="${LOCAL_REPO_DIR}/target/*.jar"
 # Remote paths (JAR + logs in same folder)
 REMOTE_APP_DIR="/opt/${APP_NAME}"
 REMOTE_JAR_PATH="${REMOTE_APP_DIR}/${APP_NAME}.jar"
-REMOTE_LOG_FILE="${REMOTE_APP_DIR}/${APP_NAME}.log"
+REMOTE_LOG_FILE="${REMOTE_APP_DIR}/log/${APP_NAME}.log"
 
 if [ -z "$PASSWORD" ] || [ -z "$BRANCH" ]; then
   echo "Usage: $0 <server-password> <branch>"
